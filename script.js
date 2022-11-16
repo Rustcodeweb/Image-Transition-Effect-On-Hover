b296bca73ca86058bce5a562d9f9e2ac8ec2a489
Rustcode <rustcode95@gmail.com>
2022-11-16 20:57:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+new hoverEffect({
+  parent: document.querySelector(".img-container"),
+  intensity: 0.4,
+  image1: "009.jpg",
+  image2: "010.jpg",
+  displacementImage: "transition01.jpg",
+  imagesRatio: "0.67"
+});
